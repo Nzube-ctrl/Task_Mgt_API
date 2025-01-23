357bebdd447646ef2a5a4b09550c35340e621ac5
Nzube Uwakwe <nzubeuwakwe@gmail.com>
2025-01-23 22:57:49 +0300

    Started documentation

--- src/Dto/create.task.dto.ts
+++ src/Dto/create.task.dto.ts
@@ -1,37 +1,46 @@
 import { IsString, IsNotEmpty, IsDateString, IsOptional, IsIn, IsUUID, IsArray } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateTaskDto {
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()//can add example data in an object
     title: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     description: string;
 
     @IsDateString()
     @IsNotEmpty()
+    @ApiProperty()
     dueDate: Date;
 
     @IsString()
     @IsOptional()
+    @ApiProperty()
     status: string;
 
     @IsString()
     @IsIn(['low', 'medium', 'high'])
     @IsOptional()
+    @ApiProperty()
     priority?: string;
 
     @IsUUID()
     @IsOptional()
+    @ApiProperty()
     createdBy?: string;
 
     @IsString()
     @IsOptional()
+    @ApiProperty()
     assignedTo?: string;
 
     @IsArray()
     @IsString({ each: true })
     @IsOptional()
+    @ApiProperty({ required: false })//can also add a description here or ApiPropertyOptional decorator
     tags?: string[];
 }
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -4,6 +4,7 @@ import { AuthenticationGuard } from 'src/guards/authentication.guard';
 // import { AuthorizationGuard } from 'src/guards/authorization.guard';
 import { CreateTaskDto } from 'src/Dto/create.task.dto';
 import { UpdateTaskDto } from 'src/Dto/update.task.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiBadRequestResponse } from '@nestjs/swagger';
 
 
 @UseGuards(AuthenticationGuard)
@@ -12,6 +13,8 @@ export class TaskController {
     constructor(private taskService: TaskService) { }
 
     @Post('')
+    @ApiOperation({ summary: `Create a task` })
+    @ApiCreatedResponse({ description: `Task created successfully`, type: CreateTaskDto })
     async createTask(@Body(ValidationPipe) createTaskDto: CreateTaskDto, @Req() req: any) {
         const createdBy = req.user.id;
         const task = await this.taskService.createTask(createTaskDto, createdBy);
@@ -19,24 +22,32 @@ export class TaskController {
     }
 
     @Get('')
+    @ApiOperation({ summary: `Get all tasks` })
+    @ApiOkResponse({ description: `Getting all tasks in the database` })
     async getAllTasks() {
         const tasks = await this.taskService.getAllTasks();
         return { message: `Tasks retrieved successfully!`, data: tasks }
     }
 
     @Get(':id')
+    @ApiOperation({ summary: `Getting a single task by id` })
+    @ApiOkResponse({ description: `Getting a single task from the database` })
     async getTaskById(@Param('id') id: string) {
         const task = await this.taskService.getTaskById(id);
         return { message: `Task retrieved successfully!`, data: task }
     }
 
     @Put(':id')
+    @ApiOperation({ summary: `Updating a single task` })
+    @ApiOkResponse({ description: `Updating a single task ` })
     async updateTask(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
         const updatedTask = await this.taskService.updateTask(id, updateTaskDto);
         return { message: `Task updated successfully!`, data: updatedTask }
     }
 
     @Delete(':id')
+    @ApiOperation({ summary: `Deleting a task` })
+    @ApiOkResponse({ description: `Deleting a task` })
     async deleteTask(@Param('id') id: string) {
         await this.taskService.deleteTask(id);
         return { message: `Task deleted successfully!` }
